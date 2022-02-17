3cdffd59b0975434b22a7b365138a42108ddc4e2
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2022-02-17 14:17:06 +0100

    ajout du tableau statistique (min,max,avg) par zones

--- back/projet_cir3_symf/src/Controller/AccueilController.php
+++ back/projet_cir3_symf/src/Controller/AccueilController.php
@@ -36,6 +36,7 @@ class AccueilController extends AbstractController
      */
     public function recherche(EntityManagerInterface $entityManager): Response
     {
+        // On vérifie l'URI
         $zone_id = $_GET["zone"];
         $espece_id = $_GET["espece"];
         if ( (!is_numeric($zone_id)) || (!is_numeric($espece_id)))
@@ -44,9 +45,35 @@ class AccueilController extends AbstractController
                 'erreur_description' => "Les identifiants dans l'URL pour la zone ou pour l'espece ne sont pas des nombres",
             ]);
         }
+        $nb_zones = $entityManager -> createQueryBuilder("z")
+        ->select('count(:c)')
+        ->from(Zone::class,"z")
+        ->setParameter('c', "*")
+        ->getQuery()
+        ->getResult()[0][1];
+
+
+        // Récupération des noms des zones / espèces
+        $espece = $entityManager
+        ->getRepository(Espece::class)
+        ->find($espece_id)
+        ->getEspece();
+        $zone = "toutes zones";
+        if ($zone_id != 0)
+        {
+            $zone = $entityManager
+            ->getRepository(Zone::class)
+            ->find($zone_id)
+            ->getZone();
+        }
+
         
+
+        // création de la requête
+        // affichage du nb d'animaux d'une espèce particulière
+        // par date et par zone
         $requete = $entityManager -> createQueryBuilder("e")
-        ->select('e.id' , 'e.date' , 'e.nombre', 'z.zone' , 's.espece')
+        ->select('e.date', 'z.zone', 'SUM(e.nombre) as nombre')
         ->from(Echouage::class, 'e')
         ->leftJoin(
             Zone::class,
@@ -69,17 +96,93 @@ class AccueilController extends AbstractController
             ->setParameter('zone_id', $zone_id);
         }
         $requete = $requete
+        ->groupBy('e.date,z.zone')
         ->orderBy('e.date,z.zone', 'ASC')
         ->getQuery();
-
         $resultats = $requete->getResult();
 
-
+        // 2nde requête
+        // récupération d'un tableau de nombres pour chaque zone
+        $stats_par_zones = array();
+        if ($zone_id == 0)
+        {
+            // toutes zones sélectionnées
+            $i = 1;
+           $derniere_zone = $nb_zones;
+        }
+        else
+        {
+            $i = $zone_id;
+            $derniere_zone = $zone_id;
+        }
+        while($i <= $derniere_zone)
+        {
+            array_push(
+                $stats_par_zones, 
+                $this -> tab_stats_zones($entityManager, $espece_id, $i)
+            );
+            $i++;
+        }   
 
         return $this->render('accueil/recherche.html.twig', [
-            'zone_id' => $zone_id,
-            'espece_id' => $espece_id,
+            'zone' => [
+                "id" => $zone_id,
+                "zone" => $zone
+            ],
+            'espece' => [
+                "id" => $espece_id,
+                "espece" => $espece
+            ],
             'resultats' => $resultats,
+            'stats_zones' => $stats_par_zones,
         ]);
+    }
+
+    public function tab_stats_zones($entityManager, $espece_id, $zone_id)
+    {
+    
+        $tab_vals = $this -> liste_nb_dans_zone($entityManager, $espece_id, $zone_id);
+        return [
+            "zone" => $entityManager
+                ->getRepository(Zone::class)
+                ->find($zone_id)
+                ->getZone(),
+            "min" => min($tab_vals),
+            "max" => max($tab_vals),
+            "avg" => array_sum($tab_vals)/count($tab_vals)
+        ];
+    }
+
+    public function liste_nb_dans_zone($entityManager, $espece_id, $zone_id)
+    {
+        $requete = $entityManager -> createQueryBuilder("e")
+        ->select('SUM(e.nombre) as nombre')
+        ->from(Echouage::class, 'e')
+        ->leftJoin(
+            Zone::class,
+            'z',
+            \Doctrine\ORM\Query\Expr\Join::WITH,
+            'e.zone = z.id'
+        )
+        ->leftJoin(
+            Espece::class,
+            's',
+            \Doctrine\ORM\Query\Expr\Join::WITH,
+            'e.espece = s.id'
+        )
+        ->where('s.id=:espece_id')   
+        ->setParameter('espece_id', $espece_id)
+        ->andWhere('z.id=:zone_id')   
+        ->setParameter('zone_id', $zone_id)
+        ->groupBy('e.date,z.zone')
+        ->orderBy('nombre', 'ASC')
+        ->getQuery();
+
+        $resultats_sql = $requete->getResult();
+        $resultats = array();
+        foreach ($resultats_sql as $value) {
+            array_push($resultats,(int)$value["nombre"]);
+        }
+        return $resultats;
     }
 }
--- back/requetes_mysql.md
+++ back/requetes_mysql.md
@@ -1,6 +1,6 @@
 # requêtes MySQL
 
-Recherche selon la zone et l'espece : MARCHE !!!
+Recherche selon la zone et l'espece :
 ```sql
 SELECT e.id , e.date , e.nombre , z.zone , s.espece
 FROM echouage e
@@ -11,7 +11,7 @@ AND s.id = 1
 ORDER BY z.zone, s.espece, e.id ASC;
 ```
 
-Recherche selon l'espece : MARCHE !!!
+Recherche selon l'espece 
 ```sql
 SELECT e.id , e.date , e.nombre , z.zone , s.espece
 FROM echouage e
@@ -21,17 +21,7 @@ WHERE s.id = 1
 ORDER BY z.zone, s.espece, e.id ASC;
 ```
 
-Recherche selon l'espece explicite :
-```sql
-SELECT echouage.id , echouage.date , echouage.nombre , zone.zone , espece.espece
-FROM echouage
-LEFT JOIN zone ON zone.id = echouage.zone_id
-LEFT JOIN espece ON espece.id = echouage.espece_id
-WHERE espece.id = 1
-ORDER BY zone.zone, espece.espece, echouage.id ASC
-```
-
-Recherche selon la zone : MARCHE !!!
+Recherche selon la zone :
 ```sql
 SELECT e.id , e.date , e.nombre , z.zone , s.espece
 FROM echouage e
@@ -42,13 +32,77 @@ ORDER BY z.zone, s.espece, e.id ASC;
 ```
 
 
-Recherche de la quantité d'une espèce dans chaque zones
+Recherche de la quantité d'une espèce choisie dans chaque zones
 ```sql
-SELECT  s.espece, z.zone, count(*) as `nombre`
+SELECT  s.espece, z.zone, SUM(e.nombre) as `nombre`
 FROM echouage e
 LEFT JOIN zone z ON z.id = e.zone_id
 LEFT JOIN espece s ON s.id = e.espece_id
 WHERE s.id = 1
 GROUP BY z.zone
 ORDER BY z.zone, s.espece ASC;
-`
+```
+
+
+
+
+Recherche de la quantité d'une espèce choisie dans chaque zones
+```sql
+SELECT  s.espece , e.date, z.zone, SUM(e.nombre) as `nombre`
+FROM echouage e
+LEFT JOIN zone z ON z.id = e.zone_id
+LEFT JOIN espece s ON s.id = e.espece_id
+WHERE s.id = 19
+GROUP BY e.date , z.zone
+ORDER BY e.date, z.zone ASC;
+```
+
+*affichage des stats d'une année*
+```sql
+    SELECT e.date, z.zone, SUM(e.nombre)
+    FROM echouage e
+    LEFT JOIN zone z ON z.id = e.zone_id
+    LEFT JOIN espece s ON s.id = e.espece_id
+    WHERE s.id = 1
+    AND e.date = 1994
+    GROUP BY e.date , z.zone
+```
+
+Il faudra également afficher un sous tableau affichant par zone, le 
+nombre mini et maxi ainsi que la moyenne.
+
+Afficher le nb minimal par zone
+Afficher le nb maximal par zone
+Afficher la moyenne par zone
+
+
++----+--------------------------------+--------------------------------------------+
+| id | zone                           | mini        | maxi        | moyenne        |
++----+--------------------------------+-------------+-------------+----------------+
+|  1 | Méditerranée                   |
+|  2 | Manche Est - mer du Nord       |
+|  3 | Nord Atlantique - Manche Ouest |
+|  4 | Sud Atlantique                 |
++----+--------------------------------+
+
+1. Afficher 1 espèce trié par zone d'abord, puis par date
+```sql
+SELECT  s.espece , e.date, z.zone, SUM(e.nombre) as `nombre`
+FROM echouage e
+LEFT JOIN zone z ON z.id = e.zone_id
+LEFT JOIN espece s ON s.id = e.espece_id
+WHERE s.id = 1
+GROUP BY e.date , z.zone
+ORDER BY z.zone, e.date ASC;
+```
+2. afficher 1 espèce dans 1 zone
+```sql
+SELECT  SUM(e.nombre) as `nombre`
+FROM echouage e
+LEFT JOIN zone z ON z.id = e.zone_id
+LEFT JOIN espece s ON s.id = e.espece_id
+WHERE s.id = 1
+AND z.id=1
+GROUP BY e.date , z.zone
+ORDER BY nombre ASC;
+```
