5cbc7005b0da5071be049aa50fd7e1c30fc9a759
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2022-02-18 20:09:49 +0100

    ajout des endpoints echouages_espece , espece , zone

--- back/projet_cir3_symf/src/Controller/ApirestController.php
+++ back/projet_cir3_symf/src/Controller/ApirestController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Echouage;
-use App\Entity\Espece;
 use App\Entity\Zone;
+use App\Entity\Espece;
 
 /**
  * @Route("/api")
@@ -22,95 +22,110 @@ class ApirestController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('apirest/index.html.twig', [
-            'controller_name' => 'ApirestController',
-        ]);
+        $data = [
+            "l'API marche" => True,
+            "liste des endpoints" =>
+            [
+                "/api",
+                "/echouages_espece/{espece_id}",
+                "/echouages_espece/{espece_id}/zone/{zone_id}",
+                "/espece/",
+                "/espece/{espece_id}",
+                "/zone/",
+                "/zone/{zone_id}"                
+            ],
+        ];
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 
     /**
-     * @Route("/echouage", name="echouage_list")
+     * @Route("/echouages_espece/{espece_id}", name="echouages_espece")
      */
-    public function echouage_list(): Response
+    public function echouages_espece($espece_id, EntityManagerInterface $em): Response
     {
-        $em = $this->getDoctrine()->getManager();
-    
-        //recuperation de la liste des cours au travers du repository
-        $cours = $em->getRepository(Cours::Class)-> findByCoursOrderedRest();
-        //reponse du controlleur en indiquant le format json et
-        // en ajoutant la balise "Access-Control-Allow-Origin"
-        // dans l'en-tete HTTP pour eviter les probleme de refus du au CORS
+        $data = $em ->getRepository(Echouage::Class)
+                    ->get_echouages_espece($espece_id);
+
         $response = new Response();
-        $response->setContent(json_encode($cours));
+        $response->setContent(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
 
+
     /**
-     * @Route("/cours/{id}", name="cours_details")
+     * @Route("/echouages_espece/{espece_id}/zone/{zone_id}", name="echouages_espece_zone")
      */
-    public function cours_details($id): Response
+    public function echouages_espece_zone($espece_id, $zone_id, EntityManagerInterface $em): Response
     {
-        $em = $this->getDoctrine()->getManager();
-    
-        //recuperation de la liste des cours au travers du repository
-        $cours = $em->getRepository(Cours::Class)-> findByCoursId($id);
-    
-        //reponse du controlleur en indiquant le format json et
-        // en ajoutant la balise "Access-Control-Allow-Origin"
-        // dans l'en-tete HTTP pour eviter les probleme de refus du au CORS
+        $data = $em ->getRepository(Echouage::Class)
+                    ->get_echouages_espece($espece_id,$zone_id);
+
         $response = new Response();
-        $response->setContent(json_encode($cours));
+        $response->setContent(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
 
-
     /**
-     * @Route("/type_cours", name="type_cours_list")
+     * @Route("/espece", name="especes_list")
      */
-    public function type_cours_list(): Response
+    public function especes_list(EntityManagerInterface $em): Response
     {
-        // créer un ensemble route/contrôleur(méthode) et repository permettant de
-        // récupérer la liste des cours pour un type de cours 
-
-        $em = $this->getDoctrine()->getManager();
+        $data = $em ->getRepository(Espece::class) ->get();
     
-        //recuperation de la liste des cours au travers du repository
-        $type_cours = $em->getRepository(TypeCours::Class)-> findByTypeCoursOrderedRest();
-        //reponse du controlleur en indiquant le format json et
-        // en ajoutant la balise "Access-Control-Allow-Origin"
-        // dans l'en-tete HTTP pour eviter les probleme de refus du au CORS
         $response = new Response();
-        $response->setContent(json_encode($type_cours));
+        $response->setContent(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
-
 
     /**
-     * @Route("/type_cours/{id}", name="type_cours_details")
-     */    
-    public function type_cours_details($id): Response
+     * @Route("/espece/{id}", name="espece_id")
+     */
+    public function espece_id($id,EntityManagerInterface $em): Response
     {
-        $em = $this->getDoctrine()->getManager();
+        $data = $em ->getRepository(Espece::class) ->get($id);
     
-        //recuperation de la liste des cours au travers du repository
-        $cours = $em->getRepository(Cours::Class)-> findByTypeCoursId($id);
-    
-        //reponse du controlleur en indiquant le format json et
-        // en ajoutant la balise "Access-Control-Allow-Origin"
-        // dans l'en-tete HTTP pour eviter les probleme de refus du au CORS
-
-
         $response = new Response();
-        $response->setContent(json_encode($cours));
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }    
+
+    /**
+     * @Route("/zone", name="zones_list")
+     */
+    public function zones_list(EntityManagerInterface $em): Response
+    {
+        $data = $em ->getRepository(Zone::class) ->get();
+    
+        $response = new Response();
+        $response->setContent(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
 
-
+    /**
+     * @Route("/zone/{id}", name="zone_id")
+     */
+    public function zone_id($id,EntityManagerInterface $em): Response
+    {
+        $data = $em ->getRepository(Zone::class) ->get($id);
+    
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }  
 }
